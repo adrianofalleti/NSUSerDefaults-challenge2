25e7a382c42744a79f850c601e0d18393c48c216
Adriano Falleti <adrianofalleti@MacBook-Pro-di-Adriano.local>
2015-09-01 09:32:52 +0200

    ok

--- NSUserDefault challenge/ViewController.h
+++ NSUserDefault challenge/ViewController.h
@@ -1,14 +1,28 @@
 //
 //  ViewController.h
-//  NSUserDefault challenge
+//  NSUserDeafults,Segue and Protocols challenge
 //
-//  Created by Adriano Falleti on 01/09/15.
+//  Created by Adriano Falleti on 31/08/15.
 //  Copyright (c) 2015 Adriano Falleti. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
+
 @interface ViewController : UIViewController
+
+
+@property (strong, nonatomic) IBOutlet UILabel *userNameLabel;
+
+
+@property (strong, nonatomic) IBOutlet UILabel *passwordLabel;
+
+@property (strong,nonatomic) NSString *userName;
+
+@property (strong,nonatomic) NSString *passWord;
+
+
+- (IBAction)signOut:(UIButton *)sender;
 
 
 @end
